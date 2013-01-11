d9a43fc0e284da66f4915c6170620a57acaa8f48
Amanda Chappell <achappell@me.com>
2013-01-11 14:10:44 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icheckgames::Application.config.secret_token = '5b01aabd0b2b6bfcb3a9b3d41423779dbc49165420b8a6d33374595b97a6d62f2cc2c3d4a7551dcabbf86be2f9a79573e082a42ece490054d7501404925f6e03'
