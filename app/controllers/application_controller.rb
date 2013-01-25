0e156b81d6583aa970f3f321fed5f69a430f4f8c
achappell <achappell@me.com>
2013-01-25 14:46:57 -0600

    Update app/controllers/application_controller.rb
    removed hard coded twitter secrets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
-
-  unless Rails.env.production?
-    ENV['TWITTER_KEY'] = 'xvqB61RpJ7jQyIrGNQ'
-    ENV['TWITTER_SECRET'] = 'nmgnTjRnAi6JLs4nu6IXVMVoWSudG3vFPcy24yznoN8'
-  end
 end
