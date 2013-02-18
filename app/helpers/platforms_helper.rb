ca9ca012c16c78841255baa7ed54ae33ff786cef
Amanda Chappell <achappell@me.com>
2013-02-18 10:11:38 -0600

    updated platform

--- app/helpers/platforms_helper.rb
+++ app/helpers/platforms_helper.rb
@@ -31,7 +31,7 @@ end
 
 def build_platform_from_hash(platform_hash)
 
-	id = platform_hash["id"]
+	@id = platform_hash["id"]
 
 	name = platform_hash["name"]
 	if (name == nil)
@@ -42,11 +42,11 @@ def build_platform_from_hash(platform_hash)
 	developer = platform_hash["developer"]
 	rating = platform_hash["Rating"]
 
-	if Platform.where(:external_id => id.first).exists?
-		platform = Platform.find_by_external_id(id.first)
+	if Platform.where(:external_id => @id .first).exists?
+		platform = Platform.find_by_external_id(@id .first)
 	else
-		platform = Platform.create
-		platform.external_id = id.first;
+		platform = Platform.new
+		platform.external_id = @id .first;
 	end
 
 	if (name)
